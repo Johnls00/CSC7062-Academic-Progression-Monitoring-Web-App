1f68c4e434c1fce0b8935f9587a0461c5fd1c220
John Livingstone <jlivingstone08@qub.ac.uk>
2025-04-07 19:53:42 +0100

    remove debug lines

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -9,10 +9,6 @@ exports.showDashboard = async (req, res) => {
     // console.log("User ID:", userId); // Debugging line
     const studentData = await studentModel.getStudentData(userId);
 
-    // console.log("Student data:", studentData); // Debugging line
-    // const student_id = studentData[0].student_id; // Debugging line
-    // console.log("Student ID:", student_id); // Debugging line
-
     res.render("student/student-dashboard", {
       title: "Student Dashboard",
       user: req.session.user,
@@ -31,20 +27,6 @@ exports.showModules = async (req, res) => {
   const modules = await studentModel.getModulesByStudentId(
     studentData[0].student_id
   );
-
-  // for (let i = 0; i < modules.length; i++) {
-  //   const moduleId = modules[i].module_id;
-  //   const module_Info = await moduleModel.getModuleInfo(moduleId);
-  //   // console.log(`Module Info for module_id ${moduleId}:`, module_Info);
-
-  //   if (module_Info && module_Info.length > 0) {
-  //     modules[i].subject_code = module_Info[0].subject_code;
-  //     modules[i].module_title = module_Info[0].module_title;
-  //   } else {
-  //     modules[i].subject_code = "Unknown";
-  //     modules[i].module_title = "Unknown";
-  //   }
-  // }
 
   // console.log("Courses data:", courses); // Debugging line
   res.render("student/modules", {
