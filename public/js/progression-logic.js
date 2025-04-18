11eaf163700aa8f12a6c81d504510f016e240818
John Livingstone <jlivingstone08@qub.ac.uk>
2025-04-18 20:40:21 +0100

    progression logic start

---
+++ public/js/progression-logic.js
@@ -0,0 +1,34 @@
+function determineProgression(record){
+    const progression = {
+        canProgress: false,
+        reason: [],
+        resists: [],
+        contactSupportOfficer: false,
+    }
+
+    // Progression rule 1 - Pass at least 100 credits
+
+
+
+
+    // Progression rule 2 - Achieve a minimum mark of 40% in all modules
+
+
+    // progression rule 3 - must pass all core modules
+
+
+    // progression rule 4 - insure only four attempts to pass
+
+
+    // progression rule 5 - give resit opportunity
+
+
+    // progression rule 6 - core modules must be passed
+
+
+    //
+
+
+
+    return progression
+}
