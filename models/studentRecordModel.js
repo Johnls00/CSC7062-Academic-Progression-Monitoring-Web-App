9d9bb80418d7fcaa4730c727585d64952454e42d
John Livingstone <jlivingstone08@qub.ac.uk>
2025-04-18 15:01:06 +0100

    starting student record calculater model

---
+++ models/studentRecordModel.js
@@ -0,0 +1,13 @@
+const connection = require("../config/config");
+
+/**
+ * This function will gather a student records and return an object with the calculated details
+ * @param {*} studentId
+ */
+async function getStudenRecord(studentId) {
+  const studentRecord = {};
+
+  
+}
+
+module.exports = {};
