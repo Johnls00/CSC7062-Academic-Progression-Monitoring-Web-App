e8a836b1c45c860c4cb514f2174248bb45ca97f5
John Livingstone <jlivingstone08@qub.ac.uk>
2025-04-19 19:42:37 +0100

    java script doc comments added to progression logic

--- utils/progression-logic.js
+++ utils/progression-logic.js
@@ -1,6 +1,29 @@
 /**
- * 
- * @param {*} record 
+ * a function that will determine the progression decision for a student based on student record object gathered by studentRecordModel
+ * @param {*} record - example student record =
+ * student record {
+  averageMark: '82.3333',
+  averageMarkPerYear: [ { level: 'L1', avg_mark: '82.3333' } ],
+  coreModulesPassed: 'N/A',
+  studentLevel: 'L1',
+  moduleAttempts: [
+    { module_id: 110, attempt_count: '1' },
+    { module_id: 111, attempt_count: '1' },
+    { module_id: 112, attempt_count: '1' },
+    { module_id: 114, attempt_count: '1' },
+    { module_id: 139, attempt_count: '1' },
+    { module_id: 115, attempt_count: '1' }
+  ],
+  modules: [
+    { module_id: 110, result: 'pass', grade: 78 },
+    { module_id: 111, result: 'pass', grade: 91 },
+    { module_id: 112, result: 'pass', grade: 85 },
+    { module_id: 114, result: 'pass', grade: 72 },
+    { module_id: 139, result: 'pass', grade: 80 },
+    { module_id: 115, result: 'pass', grade: 88 }
+  ],
+  creditsPassed: [ { module_level: 'L1', credits_passed: 120 } ]
+}
  * @returns 
  */
 async function determineProgression(record){
@@ -11,7 +34,7 @@ async function determineProgression(record){
     const MIN_MARK_TO_PASS = 40;
     const MAX_MODULE_ATTEMPTS = 4;
     
-    //progression onbject 
+    //progression object 
     const progression = {
         canProgress: false,
         reason: [],
