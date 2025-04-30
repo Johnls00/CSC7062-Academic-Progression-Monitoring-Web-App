2a110cb3ed4c242fdbe8d1ea04e68e16fe62dcfe
John Livingstone <jlivingstone08@qub.ac.uk>
2025-04-30 21:15:58 +0100

    error handling added to progression logic to handle unexpected issues.

--- utils/progression-logic.js
+++ utils/progression-logic.js
@@ -26,79 +26,114 @@
 }
  * @returns student progression object
  */
-async function determineProgression(record){
-    // required models 
-    const moduleModel = require("../models/moduleModel");
-    // constants for progression rules 
-    const MIN_CREDITS_TO_PASS = 100;
-    const MIN_MARK_TO_PASS = 40;
-    const MAX_MODULE_ATTEMPTS = 4;
+async function determineProgression(record) {
+  // required models
+  const moduleModel = require("../models/moduleModel");
+  // constants for progression rules
+  const MIN_CREDITS_TO_PASS = 100;
+  const MIN_MARK_TO_PASS = 40;
+  const MAX_MODULE_ATTEMPTS = 4;
 
-    //progression object 
-    const progression = {
-        canProgress: false,
-        reason: [],
-        resits: [],
-        contactSupportOfficer: false,
-        outcome: [],
-    }
-
+  //progression object
+  const progression = {
+    canProgress: false,
+    reason: [],
+    resits: [],
+    contactSupportOfficer: false,
+    outcome: [],
+  };
+  try {
     if (record.modules.length === 0) {
-        progression.reason.push("No module data for Student.");
-        progression.outcome.push("Student cannot progress to next level.");
-        console.log(progression) // debug line 
-        return progression;
+      progression.reason.push("No module data for Student.");
+      progression.outcome.push("Student cannot progress to next level.");
+      console.log(progression); // debug line
+      return progression;
     } else {
+      const levelCredits = record.creditsPassed.find(
+        (item) => item.module_level === record.studentLevel
+      );
+      console.log("level credits", levelCredits);
 
-        const levelCredits = record.creditsPassed.find(item => item.module_level === record.studentLevel);
-        console.log("level credits", levelCredits)
-        // const levelAverage = record.averageMarkPerYear.find(item => item.level === record.studentLevel);
+      // Progression rule 1 - Pass at least 100 credits
+      if (levelCredits.creditsPassed < MIN_CREDITS_TO_PASS && levelCredits) {
+        progression.reason.push(
+          "Insufficient credits achieved at " +
+            record.studentLevel +
+            ". (minimum " +
+            MIN_CREDITS_TO_PASS +
+            " required)"
+        );
+      }
 
-        // Progression rule 1 - Pass at least 100 credits
-        if(levelCredits.creditsPassed < MIN_CREDITS_TO_PASS && levelCredits ){
-            progression.reason.push("Insufficient credits achieved at " + record.studentLevel + ". (minimum " + MIN_CREDITS_TO_PASS +  " required)");
+      // Progression rule 2 - Achieve a minimum mark of 40% in all modules
+      for (const module of record.modules) {
+        if (module.grade != null && module.grade < MIN_MARK_TO_PASS) {
+          const [moduleInfo] = await moduleModel.getModuleInfo(
+            module.module_id
+          );
+          progression.resits.push(
+            moduleInfo.subject_code + moduleInfo.subject_catalog
+          );
+          progression.reason.push(
+            "Student must resit module " +
+              moduleInfo.subject_code +
+              moduleInfo.subject_catalog
+          );
         }
+      }
 
-        // Progression rule 2 - Achieve a minimum mark of 40% in all modules
-        for (const module of record.modules) {
-            if (module.grade != null && module.grade < MIN_MARK_TO_PASS){
-                const [moduleInfo] = await moduleModel.getModuleInfo(module.module_id);
-                progression.resits.push(moduleInfo.subject_code+moduleInfo.subject_catalog);
-                progression.reason.push("Student must resit module " + moduleInfo.subject_code+moduleInfo.subject_catalog);
-            }
-        };
+      // progression rule 3 - must pass all core modules
+      if (record.coreModulesPassed === "0") {
+        progression.reason.push("Core modules have not all been passed.");
+      }
 
-        // progression rule 3 - must pass all core modules
-        if (record.coreModulesPassed === '0'){
-            progression.reason.push("Core modules have not all been passed.")
+      // progression rule 4 - insure only four attempts to pass
+      for (const moduleAttempts of record.moduleAttempts) {
+        if (moduleAttempts.attempt_count >= MAX_MODULE_ATTEMPTS) {
+          const moduleResult = modules.find(
+            (module) => module.id === moduleAttempts.module_id
+          );
+          if (
+            moduleResult.result === "fail" ||
+            moduleResult.result === "absent"
+          ) {
+            const [moduleInfo] = await moduleModel.getModuleInfo(
+              module.module_id
+            );
+            progression.contactSupportOfficer = true;
+            progression.reason.push(
+              "Max attempts reached for " +
+                moduleInfo.subject_code +
+                moduleInfo.subject_catalog
+            );
+          }
         }
+      }
 
-        // progression rule 4 - insure only four attempts to pass
-        for (const moduleAttempts of record.moduleAttempts) {
-            if (moduleAttempts.attempt_count >= MAX_MODULE_ATTEMPTS){
-                const moduleResult = modules.find(module => module.id === moduleAttempts.module_id);
-                if (moduleResult.result === "fail" || moduleResult.result === "absent"){
-                    const [moduleInfo] = await moduleModel.getModuleInfo(module.module_id);
-                    progression.contactSupportOfficer = true;
-                    progression.reason.push("Max attempts reached for " + moduleInfo.subject_code+moduleInfo.subject_catalog);
-                }
-            }
-        }
-
-        //final progression decision
-        if (progression.reason.length === 0){
-            progression.canProgress = true;
-            progression.outcome.push("Progress to next level.");
+      //final progression decision
+      if (progression.reason.length === 0) {
+        progression.canProgress = true;
+        progression.outcome.push("Progress to next level.");
+      } else {
+        if (progression.contactSupportOfficer === true) {
+          progression.outcome.push(
+            "Please contact your Student Support Officer."
+          );
         } else {
-            if (progression.contactSupportOfficer === true){
-                progression.outcome.push("Please contact your Student Support Officer.");
-            } else {
-                progression.outcome.push("Student cannot progress to next level.")
-            }
+          progression.outcome.push("Student cannot progress to next level.");
         }
-        console.log(progression) // debug line
-        return progression
+      }
+      console.log(progression); // debug line
+      return progression;
     }
+  } catch (error) {
+    console.error("Error in determineProgression:", error);
+    progression.reason.push(
+      "An unexpected error occurred while evaluating progression please contact the admin for more details."
+    );
+    progression.outcome.push("Progression decision could not be determined.");
+    return progression;
+  }
 }
 
 module.exports = { determineProgression };
