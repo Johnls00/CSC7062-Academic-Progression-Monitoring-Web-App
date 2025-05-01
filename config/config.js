b7f2a80c88bf91cc1f7def80e89c45d2ea8917b7
John Livingstone <jlivingstone08@qub.ac.uk>
2025-05-01 22:14:09 +0100

    db connection update

--- config/config.js
+++ config/config.js
@@ -1,14 +1,17 @@
+// Establish connection with the mysql database
 const mysql = require("mysql2/promise");
 
+// connection settings
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',       // '' XAMPP or 'root' MAMP
-    database: 'APMS_TEST',       // B name
+    database: '40430805',       // B name
     port: '8889', 
     connectionLimit: 10,          // XAMPP 3306 or 8889 MAMP
 });
 
+// error handler
 connection.getConnection((err)=>{
     if(err) return console.log(err.message);
     console.log("connected to local mysql db");
